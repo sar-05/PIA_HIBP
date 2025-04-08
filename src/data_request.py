92025774be327d0659e9bb0246e283fa75c6ce2d
SAR <sebastian.alighierir@uanl.edu.mx>
2025-04-08 01:56:40 -0600

    Added proper GET request to get a colum of the data

--- src/data_request.py
+++ src/data_request.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 app_id = os.getenv('APPLICATION_ID')
 tenant_id = os.getenv('TENANT_ID')
- 
-endpoint = 'https://graph.microsoft.com/v1.0/me'
+item_id = os.getenv('ITEM_ID')
+worksheet_id = os.getenv('WORKSHEET_ID')
+
+endpoint = f"https://graph.microsoft.com/v1.0/me/drive/items/{item_id}/workbook/worksheets/{worksheet_id}/range(address='H1:H5')?$select=text"
 authority_url = f'https://login.microsoftonline.com/{tenant_id}'
-az_scopes = ['User.Read', 'Files.Read']
+az_scopes = ['User.Read', 'Files.Read', 'Files.ReadWrite']
 
 access_token = ms_graph_token(app_id, authority_url, az_scopes)
 #Request to graph API
@@ -17,4 +19,5 @@ headers = {'Authorization': f'Bearer{access_token}'}
 # headers = {'Authorization': 'Bearer' + access_token}
 response = requests.get(endpoint, headers=headers)
 print(response)
-print(response.json())
+data = response.json()
+print(data['text'])
