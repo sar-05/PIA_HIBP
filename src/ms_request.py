3b7f3fb1d70f53eae9f4c88e5ca6c1ea1f23eca8
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-02 00:09:42 -0600

    Restructured project Deleted unused data and logs directories. Renamed scripts to they haveclearer names. Created test directory for possible implementation of unit testing.

---
+++ src/ms_request.py
@@ -0,0 +1,8 @@
+#This script should define functons to retrieve all needed data from the 
+#spreadsheet.
+
+#Those functions should recieve the access_token, endpoint and headers
+#Here, validation of the output of those functions should also ne defined
+
+
+
