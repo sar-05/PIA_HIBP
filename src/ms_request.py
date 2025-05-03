23393bf6eca87fb861717708c281b79f49c367d8
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-02 20:57:55 -0600

    Functional ms_excel_request function. Renamed and moved test. Deleted ms_auth test

--- src/ms_request.py
+++ src/ms_request.py
@@ -5,14 +5,12 @@ import requests
 #Those functions should recieve the access_token, endpoint and headers
 #Here, validation of the output of those functions should also ne defined
 
-def ms_excel_request(item_id, worksheet_id, access_token, adress='G2:AK1048576'):
+def ms_excel_request(item_id, worksheet_id, access_token, address="'G2:AK1048576'"):
     graph_url = "https://graph.microsoft.com/v1.0/"
     service = f"me/drive/items/{item_id}/workbook/worksheets/{worksheet_id}/"
-    excel_range = f"range(address={adress})?$select=text"
+    excel_range = f"range(address={address})/usedRange?$select=text"
     headers = {'Authorization': f'Bearer{access_token}'}
     endpoint = f"{graph_url}{service}{excel_range}"
-    print("-----------------")
-    print(endpoint)
-    print("-----------------")
-    print(type(endpoint))
-    return requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=headers)
+    data = response.json()
+    return data['text']
