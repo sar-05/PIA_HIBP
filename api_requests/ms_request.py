0ae5d5bb1564d8e5442ab6ba975493f31f055285
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-07 02:49:01 -0600

    imported from custom module

--- api_requests/ms_request.py
+++ api_requests/ms_request.py
@@ -1,13 +1,12 @@
 import requests
 import os
+from config import RAW_DIR
 
 def ms_excel_request(item_id, access_token):
     graph_url = "https://graph.microsoft.com/v1.0/"
     endpoint = f"{graph_url}me/drive/items/{item_id}/content"
     headers = {'Authorization': f'Bearer{access_token}'}
-    save_location = os.getcwd()
+    save_location = RAW_DIR
     response = requests.get(endpoint, headers=headers)
     with open(os.path.join(save_location, "datos_encuesta.xlsx"), 'wb') as _f:
         _f.write(response.content)
-
-print(os.path.dirname(os.getcwd()))
