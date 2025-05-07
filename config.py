45cdc4dbd5d1e16d555b7379feee6cc3efc654e4
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-07 00:47:12 -0600

    Created main path variables

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve()
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DIR = DATA_DIR / "raw"
+PROCESSED_DIR = DATA_DIR / "processed"
+
+RAW_DIR.mkdir(exist_ok=True)
+PROCESSED_DIR.mkdir(exist_ok=True)
