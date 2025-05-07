47abfea9accc9fe00084de1bc40eea1e10a83fc5
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-07 04:21:58 -0600

    First working implementation of ms_request script, save state before project reorganization

--- tests/test_ms_request.py
+++ tests/test_ms_request.py
@@ -1,23 +1,15 @@
-import requests
-from ms_auth import ms_graph_token
+#!/usr/bin/env python
 import os
 from dotenv import load_dotenv
+from api_requests.ms_request import ms_excel_request
+from api_requests.ms_auth import ms_graph_token
 
 load_dotenv('.env')
 app_id = os.getenv('APPLICATION_ID')
 tenant_id = os.getenv('TENANT_ID')
 item_id = os.getenv('ITEM_ID')
-worksheet_id = os.getenv('WORKSHEET_ID')
+az_scopes = ['Files.Read']
 
-endpoint = f"https://graph.microsoft.com/v1.0/me/drive/items/{item_id}/workbook/worksheets/{worksheet_id}/range(address='H1:H5')?$select=text"
-authority_url = f'https://login.microsoftonline.com/{tenant_id}'
-az_scopes = ['User.Read', 'Files.Read', 'Files.ReadWrite']
+token = ms_graph_token(app_id, tenant_id, az_scopes)
+ms_excel_request(item_id, token)
 
-access_token = ms_graph_token(app_id, authority_url, az_scopes)
-#Request to graph API
-headers = {'Authorization': f'Bearer{access_token}'}
-# headers = {'Authorization': 'Bearer' + access_token}
-response = requests.get(endpoint, headers=headers)
-print(response)
-data = response.json()
-print(data['text'])
