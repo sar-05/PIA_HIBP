697592e8f0a5afc2e085cfd97803fab0995993e1
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-06 19:13:43 -0600

    Removed unused scripts

--- src/validators.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def validate_email(email, email_pattern):
-    mo = email_pattern.serch(email)
-    if mo == None:
-        raise ValueError(f"Invalid email format{email}")
-    return email    
