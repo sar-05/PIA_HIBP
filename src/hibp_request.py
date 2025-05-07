799827c0a909aa59eb0a3311e287ecd584f818e0
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-06 21:34:39 -0600

    Added logic to create the breach_Num column

--- src/hibp_request.py
+++ src/hibp_request.py
@@ -2,6 +2,7 @@
 from os import getenv
 from dotenv import load_dotenv
 from requests import get
+from time import sleep
 
 load_dotenv('.env')
 hibp_key = getenv('HIBP_KEY')
@@ -12,12 +13,14 @@ headers = {
     'accept' : 'application/json'
 }
 
-def hibp(email, headers=headers):
-    endpoint = f'https://haveibeenpwned.com/api/v3/breachedaccount/{email}?truncateResponse=false'
-    status_code=get(endpoint, headers=headers).status_code
+def hibp_breaches(email, headers=headers):
+    endpoint = f'https://haveibeenpwned.com/api/v3/breachedaccount/{email}'
+    response=get(endpoint, headers=headers)
+    status_code = response.status_code
     if status_code == 200:
-        return True
+        return (True, len(response.json()))
     elif status_code == 404:
-        return False
-    else:
-        return None
+        return (False, 0)
+    elif status_code == 429:
+        sleep(6)
+        return hibp_breaches(email)
--- src/pd_analyisis.py
+++ src/pd_analyisis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pandas as pd
 from time import sleep
-from hibp_request import hibp
+from hibp_request import hibp_breaches
 
 #Creation of data frame based on the Forms Excel Spreadsheet
 df = pd.read_excel("Encuesta PIA.xlsx", usecols='G:R')
@@ -14,14 +14,14 @@ indices_to_drop = []
 df['breached'] = None
 df['brech_num'] = None
 for index in df.index:
-    response = hibp(df.loc[index, 'main_email'])
+    email = df.loc[index, 'main_email'] 
+    response = hibp_breaches(email)
     if response is not None:
-        df.loc[index, 'breached'] = response
-        if response is True:
-            df.loc
+        df.loc[index, 'breached'] = response[0]
+        df.loc[index, 'brech_num'] = response[1]
     else:
         indices_to_drop.append(index)
     sleep(6)
 df.drop(indices_to_drop, axis=0, inplace=True)
 
-
+print(df)
